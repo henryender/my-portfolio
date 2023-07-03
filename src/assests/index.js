1ef85f1cb1768a3672f7ab888c39a0c0b70289e0
HenryEnder <anamezietoby@gmail.com>
2023-07-03 14:09:00 +0100

    .

--- src/assests/index.js
+++ src/assests/index.js
@@ -1,15 +1,10 @@
 import logo from "./images/logo.png";
 import bannerImg from "./images/bannerImg.png";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
-import quote from "./images/testmonial/quote.png"
 import contactImg from "./images/contact/contactImg.png"
 
 export {
   logo,
   bannerImg,
-   testimonialOne,
-  testimonialTwo,
-  quote,
+   
   contactImg
 };
